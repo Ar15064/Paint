6cefb9b3509e1ab676038862bbbe6b403473f5f7
Alek <alekramirez65@gmail.com>
2023-08-23 23:16:00 -0600

    Making a editor table for changing the brush and size

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,36 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.nio.Buffer;
 
-public class MainPanel extends JPanel implements MouseMotionListener, MouseListener {
+
+public class MainPanel extends JPanel implements MouseMotionListener, ActionListener{
+
     JLabel showingLocation = new JLabel("");
 
     int storeX = 751;
     int storeY = 346;
-    JLabel brush;
 
     JButton clear = new JButton("Click to Clear");
     MainPanel() {
         this.setVisible(true);
+        this.setLayout(new GridBagLayout());
         this.setBackground(Color.WHITE);
         this.addMouseMotionListener(this);
-        this.addMouseListener(this);
-        this.add(showingLocation);
+
+        clear.addActionListener(this);
         ImageIcon brushI = new ImageIcon("C:\\Users\\alekr\\IdeaProjects\\Paint\\src\\rec\\PaintBrush.png");
-        brushI.setImage(brushI.getImage().getScaledInstance(35,35,Image.SCALE_DEFAULT));
+        brushI.setImage(brushI.getImage().getScaledInstance(40,40,Image.SCALE_DEFAULT));
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Image image = toolkit.getImage("C:\\Users\\alekr\\IdeaProjects\\Paint\\src\\rec\\PaintBrush.png");
-        Cursor c = toolkit.createCustomCursor(image , new Point(this.getX(),
-                this.getY()), "img");
-        this.setCursor (c);
+        Cursor c = toolkit.createCustomCursor(image , new Point(this.getX(),this.getY()), "img");
+        this.setCursor(c);
 
+        addingStuff();
     }
+
+
+    public void addingStuff() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.NORTHWEST;
+        constraints.weighty = 1;
+        constraints.weightx = 2;
+        constraints.gridy = 0;
+        this.add(showingLocation,constraints);
+
+        constraints.weighty = 30;
+        constraints.weightx = 2;
+        constraints.gridy = 1;
+        this.add(clear,constraints);
+    }
+
 
 
         @Override
@@ -55,11 +69,9 @@ public class MainPanel extends JPanel implements MouseMotionListener, MouseListe
             int store1 = 0;
             int store2 = 0;
             for (int i = 0; i < help.length(); i++) {
-                if(help.charAt(i) == 'x') {
+                if (help.charAt(i) == 'x') {
                     store1 = i;
-                }
-
-                else if(help.charAt(i) == ']') {
+                } else if (help.charAt(i) == ']') {
                     store2 = i;
 
                     break;
@@ -68,27 +80,14 @@ public class MainPanel extends JPanel implements MouseMotionListener, MouseListe
 
             return help.substring(store1, store2);
         }
-        @Override
-        public void mouseClicked(MouseEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-        }
-        @Override
-        public void mousePressed(MouseEvent e) {
+        System.out.println(e.getActionCommand());
 
-        }
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-            System.out.println("no more");
-        }
-        @Override
-        public void mouseEntered(MouseEvent e) {
-        }
-        @Override
-        public void mouseExited(MouseEvent e) {
+        if(e.getActionCommand().equals(clear.getText())) {
+            repaint();
         }
 
-
-
-
+    }
 }
